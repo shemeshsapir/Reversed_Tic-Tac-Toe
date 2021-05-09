444a44d606cb9fa350adc344a2858c3fd5790f16
Sapir Turgeman <39224974+shemeshsapir@users.noreply.github.com>
2021-05-09 17:04:34 +0300

    Add files via upload

---
+++ ReversedTicTacToe/Player.cs
@@ -0,0 +1,45 @@
+﻿namespace ReversedTicTacToe
+{
+    public class Player
+    {
+        private readonly GameBoard.eSymbols r_PlayerSymbol;
+        private readonly string r_PlayerName;
+        private int m_PlayerScore;
+
+        public Player(string i_PlayerName, GameBoard.eSymbols i_PlayerSymbol)
+        {
+            r_PlayerName = i_PlayerName;
+            r_PlayerSymbol = i_PlayerSymbol;
+            m_PlayerScore = 0;
+        }
+
+        public string PlayerName
+        {
+            get
+            {
+                return r_PlayerName;
+            }
+        }
+
+        public GameBoard.eSymbols PlayerSymbol
+        {
+            get
+            {
+                return r_PlayerSymbol; 
+            }
+        }
+
+        public int PlayerScore
+        {
+            get
+            {
+                return m_PlayerScore;
+            }
+
+            set
+            {
+                m_PlayerScore = value;
+            }
+        }
+    }
+}
---
+++ ReversedTicTacToe/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ReversedTicTacToe
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            ManageGame manageGame = new ManageGame();
+            manageGame.StartGame();
+        }
+    }
+}
